681cf2a1cba82f8d7322159be0f9c81cefa70b1f
Sudhanshu Mishra <snmishra@mit.edu>
2015-10-03 15:18:12 -0400

    Created XCode File

---
+++ Ping/Ping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ping
+//
+//  Created by Sudhanshu Nath Mishra on 10/3/15.
+//  Copyright (c) 2015 Shreyanshu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
